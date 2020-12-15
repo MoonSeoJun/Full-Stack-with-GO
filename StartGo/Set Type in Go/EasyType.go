03e8f8127b0722a41a77622445b0bc4e438273c2
MunSeoJun <msj00130@gmail.co>
2020-12-15 20:07:22 +0900

    Set Type in Go

---
+++ StartGo/Set Type in Go/EasyType.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var Num int = 10
+	var Name string = "Mun Seo Jun"
+	var True bool = false
+
+	var (
+		Count = 50
+		Type  = "fire"
+		False = false
+	)
+
+	fmt.Println(Num)
+	fmt.Println(Name)
+	fmt.Println(True)
+
+	fmt.Println(Count)
+	fmt.Println(Type)
+	fmt.Println(False)
+
+	PhoneNum := 301210301
+	Like := "water"
+	Dog := false
+
+	fmt.Println(PhoneNum)
+	fmt.Println(Like)
+	fmt.Println(Dog)
+}
